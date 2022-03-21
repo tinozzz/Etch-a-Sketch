11f4ff50af1a32876d32bb05b78f6db793015c0a
Matthew Tolentino <tolentinolmatthew@gmail.com>
2022-03-21 01:15:43 -0400

    Refactor grid creation and add colour change
    Grid creation code portion modified to accomodate for size scalability.
    Initially needed to manually change width for change in rows and
    columns. Now each square uses flex-basis and creates grid using nested-
    for loop.
    Add functionality to colour in grid squares and a clear button to reset
    grid using event listeners.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,65 @@
+// checks if the mouse is pressed down
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 const body = document.querySelector("body");
-const container = document.createElement("div");
-container.setAttribute('class', 'container');
-body.appendChild(container);
-const size = 16;
-for(let i = 1; i <= size**2; i++){
+
+// grid size
+let size = 16;
+
+// old version of creating grid, square width needed to be manually fixed
+
+// const container = document.createElement("div");
+// container.setAttribute('class', 'container');
+// body.appendChild(container);
+
+// for(let i = 1; i <= size**2; i++){
     // create a new square div on each iteration
-    const square = document.createElement("div");
-    square.textContent = `${i}`;
-    square.setAttribute('style', 'border: 1px solid black');
-    square.setAttribute('class','square');
-    // change the width of each square to accomodate for the number of columns
-    square.style.width = `${100 / size - 0.5}%`;
-    container.appendChild(square);
+    // const square = document.createElement("div");
+    // square.textContent = `${i}`;
+    // square.setAttribute('style', 'border: 1px solid black');
+    // square.setAttribute('class','square');
+    // // change the width of each square to accomodate for the number of columns
+    // square.style.width = `${100 / size - 0.25}%`;
+    // container.appendChild(square);
+// }
+
+// create a grid through a nested for-loop for making rows and columns to account
+// for managing square width size of flex-wrap
+for (let i = 1; i <= size; i++){
+    // create row
+    const container = document.createElement("div");
+    container.setAttribute('class', 'container');
+    body.appendChild(container);
+    for (let j = 1; j <= size; j++){
+        // create a new square div on each row
+        const square = document.createElement("div");
+        // square.textContent = `${i}'${j}`;
+        square.setAttribute('style', 'border: 1px solid black');
+        square.setAttribute('class','square');
+        container.appendChild(square);
+    }
 }
+
+function colourIn(e) {
+    if (e.type ==='mouseover' && !mouseDown) return;
+    console.log(e.type);
+    this.classList.add('testingClass');
+}
+
+function colourOut() {
+    for (const box of shade){
+        box.classList.remove('testingClass');
+    }
+}
+const shade = document.querySelectorAll('.square');
+shade.forEach(box =>{
+    box.addEventListener("mousedown", colourIn);
+    box.addEventListener("mouseover", colourIn);
+});
+
+const button = document.createElement("button");
+button.textContent = "clear";
+button.addEventListener("click", colourOut);
+body.insertBefore(button, body.children[0]);
